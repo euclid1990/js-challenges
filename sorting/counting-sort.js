9bc646fe5e146625403e10457d3e30e62699c3f6
nipunbharti <ced16i026@iiitdm.ac.in>
2018-10-02 17:57:56 +0530

    Travis CI passing

--- sorting/counting-sort.js
+++ sorting/counting-sort.js
@@ -13,25 +13,26 @@
  * @return {Array} Sorted array.
  */
 
-function countingSort(arr, min, max)
-  {
-    var i, z = 0, count = [];
- 
-    for (i = min; i <= max; i++) {
-        count[i] = 0;
-    }
- 
-    for (i=0; i < arr.length; i++) {
-        count[arr[i]]++;
-    }
- 
-    for (i = min; i <= max; i++) {
-        while (count[i]-- > 0) {
-            arr[z++] = i;
-        }
-    }
+function countingSort(arr, min, max) {
+  var i;
+  var z = 0;
+  var count = [];
 
-    return arr;
+  for (i = min; i <= max; i++) {
+    count[i] = 0;
+  }
+
+  for (i = 0; i < arr.length; i++) {
+    count[arr[i]]++;
+  }
+
+  for (i = min; i <= max; i++) {
+    while (count[i]-- > 0) {
+      arr[z++] = i;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = countingSort;
