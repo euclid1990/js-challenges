7f656c62d30ca6f8b05247b233826d494ed46e33
boat46450 <boat46450@hotmail.com>
2018-10-02 15:11:50 +0700

    insertion sort

---
+++ sorting/insertion-sort.js
@@ -0,0 +1,40 @@
+/**
+ * Implement [Insertion sort]
+ *
+ * Copyright (c) 2018, Puripat Arayasirikul.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+function swap(arr, i, j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
+
+/**
+ * Insertion sort algorithm.
+ * Complexity: O(N^2).
+ *
+ * @example
+ * console.log(insertionSort([2, 5, 1, 0, 4])); // [ 0, 1, 2, 4, 5 ]
+ *
+ * @public
+ * @module sorting/insertionsort
+ * @param {Array} array Input array.
+ * @return {Array} Sorted array.
+ */
+function insertionSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    let value = array[i]
+    let index = i - 1;
+    while (index >= 0 && array[index] > value) {
+      array[index + 1] = array[index];
+      array[index--] = value;
+    }
+  }
+  return array;
+}
+
+module.exports = insertionSort;
