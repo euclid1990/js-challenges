ddd98be251fafdde6a9650b3e8a1e1213879927f
SergioMD96 <sergio@zapiens.org>
2018-10-02 11:16:34 +0200

    Added counting sort algorithm

---
+++ sorting/counting-sort.js
@@ -0,0 +1,28 @@
+/**
+ * Implement [Counting sort]
+ *
+ * Copyright (c) 2018, Sergio Mosquera.
+ * Licensed under the MIT License.
+ */
+
+function countingSort(arr, min, max)
+  {
+    var i, z = 0, count = [];
+ 
+    for (i = min; i <= max; i++) {
+        count[i] = 0;
+    }
+ 
+    for (i=0; i < arr.length; i++) {
+        count[arr[i]]++;
+    }
+ 
+    for (i = min; i <= max; i++) {
+        while (count[i]-- > 0) {
+            arr[z++] = i;
+        }
+    }
+ return arr;
+}
+
+module.exports = countingSort;
