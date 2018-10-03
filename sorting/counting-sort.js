77a02d02ddc784b6eda2680bcb9aab3454745b08
SergioMD96 <sergio@zapiens.org>
2018-10-03 11:26:48 +0200

    Fixed conflicts on Counting sort

--- sorting/counting-sort.js
+++ sorting/counting-sort.js
@@ -5,24 +5,37 @@
  * Licensed under the MIT License.
  */
 
-function countingSort(arr, min, max)
-  {
+/**
+ * Counting sort algorithm.
+ * Complexity: O(N).
+ *
+ * @example
+ * console.log(countingSort([2, 5, 1, 0, 4]),0,5); // [ 0, 1, 2, 4, 5 ]
+ *
+ * @public
+ * @module sorting/counting-sort
+ * @param {Array} arr Input array.
+ * @param {Integer/Float/Char} min Input minimum value of array.
+ * @param {Integer/Float/Char} max Input maximum value of array.
+ * @return {Array} Sorted array.
+ */
+function countingSort(arr, min, max) {
     var i, z = 0, count = [];
- 
+
     for (i = min; i <= max; i++) {
         count[i] = 0;
     }
- 
-    for (i=0; i < arr.length; i++) {
+
+    for (i = 0; i < arr.length; i++) {
         count[arr[i]]++;
     }
- 
+
     for (i = min; i <= max; i++) {
         while (count[i]-- > 0) {
             arr[z++] = i;
         }
     }
- return arr;
+    return arr;
 }
 
 module.exports = countingSort;
