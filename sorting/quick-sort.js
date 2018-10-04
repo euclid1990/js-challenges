51c671dbc5bcb1eb0bb91e76c29c923b5d2ee17e
Ashok Subedi <ashok.subedi@SPI.COM>
2018-10-04 15:16:03 +0545

    quick sort format according to Eslint

--- sorting/quick-sort.js
+++ sorting/quick-sort.js
@@ -27,24 +27,21 @@
  * @return {Array} Sorted array.
  */
 function quickSort(origArray) {
- if (origArray.length <= 1) { 
+  if (origArray.length <= 1) {
     return origArray;
   } else {
-
     var left = [];
     var right = [];
     var newArray = [];
     var pivot = origArray.pop();
-    var length = origArray.length;
 
-    for (var i = 0; i < length; i++) {
+    for (var i = 0; i < origArray.length; i += 1) {
       if (origArray[i] <= pivot) {
         left.push(origArray[i]);
       } else {
         right.push(origArray[i]);
       }
     }
-
     return newArray.concat(quickSort(left), pivot, quickSort(right));
   }
 }
