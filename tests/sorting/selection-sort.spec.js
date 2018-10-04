f41222e3b26b5e9cfde1b83d1b761e84af2e10c6
Andrew Robilliard <andrew.robilliard@intergen.co.nz>
2018-10-04 15:36:08 +1300

    Implemented Selection Sort

---
+++ tests/sorting/selection-sort.spec.js
@@ -0,0 +1,21 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sort = require('../../sorting/selection-sort.js');
+
+describe('Selection sort', function() {
+  it('should work with empty arrays', function() {
+    expect(sort([])).deep.equal([]);
+  });
+
+  it('should work with sorted arrays', function() {
+    expect(sort([1, 2, 3, 4])).to.have.ordered.members([1, 2, 3, 4]);
+  });
+
+  it('should work with non-sorted arrays', function() {
+    let array = [2, 5, 1, 0, 4, 3];
+    array = sort(array);
+    for (let i = 0; i < array.length - 1; i += 1) {
+      expect(array[i] <= array[i + 1]).to.be.true;
+    }
+  });
+});
