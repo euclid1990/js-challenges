3b6b23da3e12ca1ab36be60f7900d9e2bf248b6d
Arminas Katilius <arminas.katilius@devbridge.com>
2018-10-17 10:46:36 +0300

    Add additional test case for custom comparator

--- tests/sorting/bubble-sort.spec.js
+++ tests/sorting/bubble-sort.spec.js
@@ -18,4 +18,11 @@ describe('Bubble Sort', function() {
       expect(array[i] <= array[i + 1]).to.be.true;
     }
   });
+
+  it('should support custom compare function', () => {
+    const emptyStringFirst = (value) => !value ? -1 : 1;
+    expect(sort(['a', 'b', '', 'c', ''], emptyStringFirst)).to.deep.equal(
+        ['', '', 'a', 'b', 'c',]
+    )
+  });
 });
